e58e5ff3d0cd821377b061b3fadbc676a584034c
Timo Siegle <timo.siegle@bechtle.com>
2016-06-09 23:41:31 +0200

    first commit

---
+++ Peticion al servidor/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  Peticion al servidor
+//
+//  Created by Timo Siegle on 08.06.16.
+//  Copyright © 2016 Timo Siegle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // Campo de texto que permite la captura del ISBN
+    @IBOutlet weak var inputField: UITextField!
+    @IBOutlet weak var resultView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        inputField.delegate = self
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        let reachability: Reachability
+        do {
+            reachability = try Reachability.reachabilityForInternetConnection()
+        } catch {
+            return false
+        }
+        
+        if reachability.isReachable() {
+            self.fetchBookDetails(textField.text!)
+        } else {
+            
+            // En caso de falla en Internet, se muestra una alerta indicando ese problema
+            let alertController = UIAlertController(title: "Internet connection", message:
+                "Please check your connection and try again!", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+        }
+        
+        return true
+    }
+    
+    // La marca de "limpiar" en la caja de texto aparece todo el tiempo
+    @IBAction func clearInput() {
+        inputField.text = ""
+        resultView.text = ""
+    }
+    
+    func fetchBookDetails(isbn: String) {
+        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:\(isbn)"
+        let url = NSURL(string: urls)
+        let datos = NSData(contentsOfURL: url!)
+        let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+        
+        //Se muestra el resultado en concordancia al ISBN ingresado
+        self.resultView.text = texto! as String
+    }
+}
+
---
+++ Peticion al servidorTests/Peticion_al_servidorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Peticion_al_servidorTests.swift
+//  Peticion al servidorTests
+//
+//  Created by Timo Siegle on 08.06.16.
+//  Copyright © 2016 Timo Siegle. All rights reserved.
+//
+
+import XCTest
+@testable import Peticion_al_servidor
+
+class Peticion_al_servidorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
